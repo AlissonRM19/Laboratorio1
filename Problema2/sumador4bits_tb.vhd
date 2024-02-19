f61aa9fca91bbaf6cd68f5542ef2314c7bd37f2b
AlissonRM19 <88214936+AlissonRM19@users.noreply.github.com>
2024-02-19 17:03:50 -0600

    Implementacion del sumador completo en la FPGA

--- Problema2/sumador4bits_tb.vhd
+++ Problema2/sumador4bits_tb.vhd
@@ -7,60 +7,60 @@ entity sumador4bits_tb is
 -- No hay puertos de entrada/salida en el testbench.
 end sumador4bits_tb;
 
-architecture Behavioral of sumador4bits_tb is
+architecture testbench of sumador4bits_tb is
   -- Declaraciones de señales y constantes
-  signal Ent1, Ent2 : std_logic_vector(3 downto 0);
-  signal Resultado : std_logic_vector(3 downto 0);
-  signal Carry : std_logic;
+  signal Ent1_tb, Ent2_tb : std_logic_vector(3 downto 0);
+  signal Resultado_tb : std_logic_vector(3 downto 0);
+  signal Cout_tb : std_logic;
+  signal salida7seg0_tb : std_logic_vector(6 downto 0);
+  signal salida7seg1_tb : std_logic_vector(6 downto 0);
+  
 
 begin
   -- Instancia del sumador binario completo de 4 bits
   UUT: entity work.sumador4bits
     port map (
-      Ent1 => Ent1,
-      Ent2 => Ent2,
+      Ent1 => Ent1_tb,
+      Ent2 => Ent2_tb,
       --cin => cin,
-      Resultado => Resultado,
-      Carry => Carry
+      Resultado => Resultado_tb,
+      Cout => Cout_tb,
+		salida7seg0 => salida7seg0_tb,
+		salida7seg1 => salida7seg1_tb
     );
 
   -- Proceso de estímulo (stimulus process)
-  stim_process: process
+  process
   begin
-    -- Configuración de los valores iniciales
-    Ent1 <= "0000";
-    Ent2 <= "0000";
-    --cin <= '0';
 
     -- Ejemplo 1
-    Ent1 <= "1101";
-    Ent2 <= "0011";
+    Ent1_tb <= "1101";
+    Ent2_tb <= "0011";
     --cin <= '0';
     wait for 10 ns;
 
     -- Estímulo 2
-    Ent1 <= "1000";
-    Ent2 <= "1000";
+    Ent1_tb <= "1000";
+    Ent2_tb <= "1000";
     --cin <= '1';
     wait for 10 ns;
 	 
 	 -- Ejemplo 3
-    Ent1 <= "1101";
-    Ent2 <= "1111";
+    Ent1_tb <= "1101";
+    Ent2_tb <= "1111";
     --cin <= '0';
     wait for 10 ns;
 
     -- Ejemplo 4
-    Ent1 <= "1001";
-    Ent2 <= "0001";
+    Ent1_tb <= "1001";
+    Ent2_tb <= "0001";
     --cin <= '1';
     wait for 10 ns;
 
-    -- Puedes agregar más casos de prueba según sea necesario.
 	 
 
     -- Detener la simulación
     wait;
-  end process stim_process;
+  end process;
 
-end Behavioral;
+end testbench;
