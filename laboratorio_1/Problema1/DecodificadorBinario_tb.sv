284677706235a104fb9b8ee464f846fdb6328a71
Hack <38637984+Hack998@users.noreply.github.com>
2024-02-23 16:03:24 -0600

    Branch_p1

--- laboratorio_1/Problema1/DecodificadorBinario_tb.sv
+++ laboratorio_1/Problema1/DecodificadorBinario_tb.sv
@@ -1,26 +1,26 @@
 module DecodificadorBinario_tb;
 
-    // Definición de las señales de entrada y salida
-    logic [3:0] bin;
-    logic [6:0] seg;
-    integer i;
+		// Definición de las señales de entrada y salida
+		logic [3:0] bin;
+		logic [6:0] seg;
+		integer i;
 
-    // Instanciación del módulo 
-    DecodificadorBinario testbench (
-        .bin(bin), 
-        .seg(seg)
-    );
+		// Instanciación del módulo 
+		DecodificadorBinario testbench (.bin(bin), .seg(seg));
 
-    // Aplicación de los estímulos
-    initial begin
-       
-        for (i = 0; i < 8; i = i + 1) begin
-            bin = i;
-            #40; 
-        end
-        
-        //$finish;
+		// Aplicación de los estímulos
+		initial begin
+			$display("Binario | Segmentos");
+			// Demostración para 8 casos
+			for (i = 0; i < 8; i = i + 1) begin
+					bin = i;
+					#40; 
+				
+					$display("--------|----------");
+					$write("%b    | ", bin);
+					$display("%b", seg);
+			end
 		  
-    end
+		end
 	 
 endmodule
