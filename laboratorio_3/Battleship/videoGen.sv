0cd52fd44de1bd4618ecf71256944203b59f9b5a
AlissonRM19 <88214936+AlissonRM19@users.noreply.github.com>
2024-04-13 08:18:41 -0600

    Creacion de los tableros para el juego

--- laboratorio_3/Battleship/videoGen.sv
+++ laboratorio_3/Battleship/videoGen.sv
@@ -1,16 +1,178 @@
 module videoGen(input logic [9:0] x, y, output logic [7:0] r, g, b);
+    logic pixel, inrect1, inrect2;
+    rectgen rectgen1(inrect1);
 
-	logic pixel, inrect;
-	// Given y position, choose a character to display
-	// then look up the pixel value from the character ROM
-	// and display it in red or blue.Also draw a green rectangle.
-	chargenrom chargenromb(y[8:3] + 8'd65, x[2:0], y[2:0], pixel);
-	//rectgen rectgen(x, y, 10'd120, 10'd150, 10'd200, 10'd230, inrect);
-	
-	rectgen rectgen(x, y, 10'd180, 10'd190, 10'd280, 10'd280, inrect);
-	//Al modificar los valores numericos estos hacen que el rectangulo cambie de posicion
+	 always_comb begin
+	  // Asignación de color negro por defecto
+        r = 8'h00;
+        g = 8'h00;
+        b = 8'h00;
+		 if (x == 10'd25 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x == 10'd295 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x == 10'd79 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x == 10'd133 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x == 10'd187 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x == 10'd241 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 //Hasta aca son las lineas verticales del tablero 1
+		 
+		 if (x == 10'd345 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x == 10'd399 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x == 10'd453 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x == 10'd507 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x == 10'd561 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x == 10'd615 && y >= 10'd50 && y <= 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 //Hasta aqui llegan las paredes verticales del segundo tablero
+		 
+		 if (x >= 10'd345 && x <= 10'd615 && y == 10'd50) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x >= 10'd345 && x <= 10'd615 && y == 10'd120) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x >= 10'd345 && x <= 10'd615 && y == 10'd190) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x >= 10'd345 && x <= 10'd615 && y == 10'd260) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x >= 10'd345 && x <= 10'd615 && y == 10'd330) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x >= 10'd345 && x <= 10'd615 && y == 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 //Hasta aqui llegan las lineas horizontales del tablero 2
+		 
+		 if (x >= 10'd25 && x <= 10'd295 && y == 10'd50) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x >= 10'd25 && x <= 10'd295 && y == 10'd120) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x >= 10'd25 && x <= 10'd295 && y == 10'd190) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x >= 10'd25 && x <= 10'd295 && y == 10'd260) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 
+		 if (x >= 10'd25 && x <= 10'd295 && y == 10'd330) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 if (x >= 10'd25 && x <= 10'd295 && y == 10'd400) begin
+				// Si las condiciones se cumplen, pinta la línea de color blanco
+					r = 8'hFF;
+					g = 8'hFF;
+					b = 8'hFF;
+		 end
+		 //Hasta aqui llegan las lineas horizontales del tablero 1
+		 
+	end
 
-	assign {r, b} = (y[3]==0) ? {{8{pixel}},8'h00} : {8'h00,{8{pixel}}};
-	assign g = inrect ? 8'hFF : 8'h00;
-	
 endmodule
