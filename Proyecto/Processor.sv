c5be9de5e0396067e77e22ba517960378da5727a
Hack <38637984+Hack998@users.noreply.github.com>
2024-06-11 15:48:07 -0600

    Unir

--- Proyecto/Processor.sv
+++ Proyecto/Processor.sv
@@ -1,3 +1,24 @@
-module Processor ();
+module Processor (input logic clk, 
+						input logic rst,
+						input logic [31:0] writedata_b, 
+						input logic [31:0] dataadr_b, 
+						input logic memwrite_b, 
+						output logic [7:0] ascii);
 
+	logic [31:0] pc, instr, readdata;
+	arm arm(clk, rst, PC, Instr, MemWrite, DataAdr,WriteData, readdata);
+	
+	Instruction_Memory imem(.address(pc),
+									.clock(clk),
+									.q(instr));
+	
+	data_memory dmem (.address_a(),
+							.address_b(dataadr_b),
+							.clock(clk),
+							.data_a(),
+							.data_b(writedata_b),
+							.wren_a(),
+							.wren_b(memwrite_b),
+							.q_a(readdata),
+							.q_b(ascii));
 endmodule
